3d634496819a4a181c06ad2eda99b6ff589bcfb4
viktoria.klisheva <vikmah2@gmail.com>
2021-11-02 00:47:49 +0300

    ДЗ по Java, сорри, я полный 0 в Java..

---
+++ src/main/java/guru/qa/Actor.java
@@ -0,0 +1,12 @@
+package guru.qa;
+
+public class Actor {
+    public static void main(String[] args) {
+        String name = "Леонардо";
+        int age = 46;
+        String city = "USA";
+        String favorite = name + " " + (Integer.toString(46)) + " " + city;
+        System.out.println(favorite);
+
+    }
+}
---
+++ src/main/java/guru/qa/Number.java
@@ -0,0 +1,12 @@
+package guru.qa;
+
+public class Number {
+       public static void main(String[] args) {
+            int b = 3126 - 8;
+            System.out.println(b);
+
+    }
+
+
+
+}
